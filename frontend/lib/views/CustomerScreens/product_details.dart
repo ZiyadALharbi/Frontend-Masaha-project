59c8b37bf84996f2506fc846391311e96d5806d3
omar alshehri <3omr.sa@gmail.com>
2023-06-19 14:57:03 +0300

    add

--- frontend/lib/views/CustomerScreens/product_details.dart
+++ frontend/lib/views/CustomerScreens/product_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:frontend/constants/colors.dart';
 
 class ProductDetails extends StatelessWidget {
@@ -7,13 +8,98 @@ class ProductDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: white,
-        elevation: 0,
+      body: ListView(
+        children: const [
+          Stack(
+            children: [
+              Images(),
+              MyIcons(),
+              Positioned(child: Details()),
+            ],
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text("hello"),
+    );
+  }
+}
+
+class Images extends StatelessWidget {
+  const Images({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageSlideshow(
+      height: MediaQuery.of(context).size.height * 0.30,
+      indicatorColor: Colors.white,
+      indicatorRadius: 5,
+      children: const [
+        Image(
+          image: AssetImage('images/Group 71.png'),
+        ),
+        Image(
+          image: AssetImage('images/Group 68.png'),
+        ),
+        Image(
+          image: AssetImage('images/Group 72.png'),
+        ),
+      ],
+    );
+  }
+}
+
+class MyIcons extends StatelessWidget {
+  const MyIcons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 52, horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            mini: true,
+            elevation: 0,
+            backgroundColor: Colors.white,
+            child: const Icon(
+              Icons.close_outlined,
+              color: Colors.black,
+            ),
+          ),
+          Row(
+            children: [
+              const SizedBox(width: 8),
+              FloatingActionButton(
+                onPressed: () {},
+                mini: true,
+                elevation: 0,
+                backgroundColor: Colors.white,
+                child: const Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
+    );
+  }
+}
+
+class Details extends StatelessWidget {
+  const Details({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: darkBlue,
+      height: 100,
     );
   }
 }
