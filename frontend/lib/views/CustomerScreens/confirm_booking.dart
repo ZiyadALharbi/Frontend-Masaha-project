189950297f647082d6916c0442aeab55a816458b
omar alshehri <3omr.sa@gmail.com>
2023-06-20 16:11:31 +0300

    nothing

--- frontend/lib/views/CustomerScreens/confirm_booking.dart
+++ frontend/lib/views/CustomerScreens/confirm_booking.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/constants/colors.dart';
+import 'package:rive/rive.dart';
 
 class ConfirmBooking extends StatelessWidget {
   const ConfirmBooking({super.key});
@@ -7,7 +8,21 @@ class ConfirmBooking extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: orange,
-    );
+        backgroundColor: white,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(120, 100, 100, 0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: RiveAnimation.asset(
+                      'assets/animations/checkmark_icon.riv'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
