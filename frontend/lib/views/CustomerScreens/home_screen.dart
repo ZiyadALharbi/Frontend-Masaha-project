692ccaeea5b9e531de83553b63004d71629a0a38
fahad0100 <f.alazmi@safcsp.org.sa>
2023-06-21 15:05:50 +0300

    hhh

--- frontend/lib/views/CustomerScreens/home_screen.dart
+++ frontend/lib/views/CustomerScreens/home_screen.dart
@@ -107,7 +107,7 @@ class _CustomerHomeState extends State<CustomerHome> {
                                     child: CardHomeCus(
                                         card: item,
                                         imgUrl:
-                                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYIa6D7LhMk7211BoEiPhRGHFRpLUEBmWjOQ&usqp=CAU'),
+                                            item["images"].toString() ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYIa6D7LhMk7211BoEiPhRGHFRpLUEBmWjOQ&usqp=CAU"),
                                     onTap: () {
                                       context.nextPage(
                                           view: ProductDetails(product: item));
